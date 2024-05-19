67fd0b7de680739f46a0d86ccdc4420fc9ca2899
GitMoDu <batatas@gmail.com>
2024-05-19 18:22:03 +0100

    Replaced FastCrc with Crc8 (https://github.com/RobTillaart/CRC). Slightly slower, but saves ~250 bytes of ROM.

--- src/EmbeddedStorageBase/EmbeddedCrc.h
+++ src/EmbeddedStorageBase/EmbeddedCrc.h
@@ -1,34 +1,33 @@
 #ifndef _EMBEDDED_CRC_
 #define _EMBEDDED_CRC_
 
-#include <FastCRC.h>
+#include <CRC.h>
 
-#if !defined(FastCRC_h)
-#pragma Depends on https://github.com/FrankBoesing/FastCRC
+#if !defined(CRC_LIB_VERSION)
+#pragma Depends on https://github.com/RobTillaart/CRC
 #else
 /// <summary>
 /// Template based abstraction for 8 bit CRC calculation.
-/// Depends on https://github.com/FrankBoesing/FastCRC
+/// Depends on https://github.com/RobTillaart/CRC .
 /// </summary>
 /// <param name="Key">Crypto MAC key.</param>
 template<const uint8_t Key = 0>
 class EmbeddedCrc
 {
 private:
-	FastCRC8 SmBus;
-	uint8_t Seed;
+	CRC8 Crc8{};
 
 public:
 	EmbeddedCrc() {}
 
 	const uint8_t GetCrc(const uint8_t* data, const uint16_t length, const uint8_t salt = 0)
 	{
-		SmBus.smbus(data, (size_t)length);
+		Crc8.reset();
+		Crc8.add(data, (size_t)length);
+		Crc8.add(Key);
+		Crc8.add(salt);
 
-		Seed = Key;
-		SmBus.smbus_upd(&Seed, (size_t)sizeof(uint8_t));
-
-		return SmBus.smbus_upd(&salt, (size_t)sizeof(uint8_t));
+		return Crc8.calc();
 	}
 };
 #endif
